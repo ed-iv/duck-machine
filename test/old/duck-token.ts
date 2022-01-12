0a15ad67568144fe97a243a1d35a1ce98656eb18
Ed Henriques IV <ehenriquesiv@gmail.com>
2022-01-12 15:19:47 -0800

    Add duck proofs

--- test/old/duck-token.ts
+++ test/old/duck-token.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import testDuck from "../scripts/testdata/testdata";
+import testDuck from "../../scripts/testdata/testdata";
 import "hardhat-gas-reporter";
 
 describe("Duck Token", function () {
