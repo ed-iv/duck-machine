052c881136f12596afa58243b321b2e228be4b0b
Rondoor <92342281+rondoor124@users.noreply.github.com>
2022-03-15 08:17:56 -0500

    complete smart contract simplification.

--- test/duck-machine.ts
+++ test/duck-machine.ts
@@ -1,53 +1,39 @@
 /* eslint-disable node/no-extraneous-import */
 import { ethers } from "hardhat";
 import "hardhat-gas-reporter";
-import { ChainsawProjects, TheAmazingTozziDuckMachine } from "../typechain";
+import { TheAmazingTozziDuckMachine } from "../typechain";
 import duckData from "../duck-data/proofs.json";
 
 describe("TheAmazingTozziDuckMachine", function () {
-  let chainsawProjects: ChainsawProjects,
-    theAmazingTozziDuckMachine: TheAmazingTozziDuckMachine,
+  let theAmazingTozziDuckMachine: TheAmazingTozziDuckMachine,
     signer1: any,
     signer2: any;
 
   it("deploy and init the smart contracts", async function () {
     [signer1, signer2] = await ethers.getSigners();
 
-    console.log("signer1", signer1.address);
-    console.log("signer2", signer2.address);
-
-    const ChainsawProjects = await ethers.getContractFactory(
-      "ChainsawProjects",
-      signer1
-    );
-    chainsawProjects = await ChainsawProjects.deploy();
-    await chainsawProjects.deployed();
-    console.log("ChainsawProject address", chainsawProjects.address);
+    console.log("signer1 ===== ", signer1.address); // admin
+    console.log("signer2 ===== ", signer2.address); // owner
 
     const TheAmazingTozziDuckMachine = await ethers.getContractFactory(
       "TheAmazingTozziDuckMachine",
       signer1
     );
     theAmazingTozziDuckMachine = await TheAmazingTozziDuckMachine.deploy(
-      ethers.utils.getAddress(chainsawProjects.address)
+      "Test"
     );
     await theAmazingTozziDuckMachine.deployed();
     console.log(
-      "TheAmazingTozziDuckMachine address",
+      "TheAmazingTozziDuckMachine address ===== ",
       theAmazingTozziDuckMachine.address
     );
-
-    const addProjectTx = await chainsawProjects.addProject(
-      "Tozzi Ducks",
-      theAmazingTozziDuckMachine.address
-    );
-    await addProjectTx.wait();
   });
 
   it("set setting functions", async function () {
-    const setMachineSettingTx =
-      await theAmazingTozziDuckMachine.setMachineSetting({
-        tozziDuckPrice: ethers.BigNumber.from(2),
+    const setMachineSettingTx = await theAmazingTozziDuckMachine
+      // .connect(signer2)
+      .setMachineSetting({
+        tozziDuckPrice: ethers.BigNumber.from(1),
         customDuckPrice: ethers.BigNumber.from(1),
         maxCustomDucks: ethers.BigNumber.from(200),
         tozziDucksEnabled: true,
@@ -61,7 +47,7 @@ describe("TheAmazingTozziDuckMachine", function () {
     data.map(async (duck, index) => {
       const mintTozziDuckTx = await theAmazingTozziDuckMachine
         .connect(signer2)
-        .mintTozziDuck(index, duck.webp, duck.proof, { value: 2 });
+        .mintTozziDuck(index, duck.webp, duck.proof, { value: 1 });
       await mintTozziDuckTx.wait();
       console.log("duck token id ===== ", index);
     });
@@ -89,23 +75,33 @@ describe("TheAmazingTozziDuckMachine", function () {
   it("call token uri", async function () {
     const tokenURITx = await theAmazingTozziDuckMachine
       .connect(signer2)
-      .tokenURI(0);
+      .tokenURI(201);
     console.log("tokenURITx ===== ", tokenURITx);
   });
 
   it("withdraw", async function () {
     console.log(
-      "machine balance",
+      "machine balance ===== ",
       await ethers.provider.getBalance(theAmazingTozziDuckMachine.address)
     );
     const signer2Address = await signer2.getAddress();
     const withdrawTx = await theAmazingTozziDuckMachine
       .connect(signer1)
-      .withdraw(signer2Address, 0);
+      .withdraw(signer2Address, 1);
     await withdrawTx.wait();
     console.log(
-      "signer2 balance",
+      "signer2 balance ===== ",
       await ethers.provider.getBalance(signer2Address)
     );
+  });
+
+  it("verify", async function () {
+    const ownershipTokenId =
+      await theAmazingTozziDuckMachine.OWNERSHIP_TOKEN_ID();
+    console.log("Ownership Token Id ===== ", ownershipTokenId);
+    const ownership = await theAmazingTozziDuckMachine.ownerOf(
+      ownershipTokenId
+    );
+    console.log("ownership ===== ", ownership);
   });
 });
