d26a704cb9a717dbbf6a0b37d3b9bed9f0d7ef71
Ed Henriques IV <ehenriquesiv@gmail.com>
2022-06-06 15:30:29 -0700

    update deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,37 +1,25 @@
-/* eslint-disable camelcase */
-/* eslint-disable node/no-missing-import */
 import { ethers } from "hardhat";
 import { exit } from "process";
 import { BigNumber, utils } from "ethers";
-import { TheAmazingTozziDuckMachine__factory } from "../typechain";
 
 const MINTSTATUS_ENABLED = BigNumber.from(0);
 const MINTSTATUS_DISABLED = BigNumber.from(1);
 const MINTSTATUS_WHITELIST = BigNumber.from(2);
 
 const defaultConfig = {
-  tozziDuckPrice: utils.parseEther("0.1"), // tozziDuckPrice
-  customDuckPrice: utils.parseEther("0.2"), // customDuckPrice
-  maxCustomDucks: BigNumber.from(0), // maxCustomDucks
-  tozziDuckMintStatus: MINTSTATUS_ENABLED, // tozziDuckMintStatus
-  customDuckMintStatus: MINTSTATUS_ENABLED, // tozziDuckMintStatus
+  tozziDuckPrice: utils.parseEther("0.001"), 
+  customDuckPrice: utils.parseEther("0.001"),
+  maxCustomDucks: BigNumber.from(100),
+  tozziDuckMintStatus: MINTSTATUS_ENABLED,
+  customDuckMintStatus: MINTSTATUS_ENABLED,
 };
 
-async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying contracts with the account:", deployer.address);
-  const TheAmazingTozziDuckMachine = await ethers.getContractFactory(
-    "TheAmazingTozziDuckMachine"
-  );
-  const theAmazingTozziDuckMachine = await TheAmazingTozziDuckMachine.deploy(
-    defaultConfig,
-    "Ownership Token URI"
-  );
+async function main() {  
+  const TheAmazingTozziDuckMachine = await ethers.getContractFactory("TheAmazingTozziDuckMachine");
+  const theAmazingTozziDuckMachine = await TheAmazingTozziDuckMachine.deploy(defaultConfig,"foo");
   await theAmazingTozziDuckMachine.deployed();
-  console.log(
-    "TheAmazingTozziDuckMachine deployed to:",
-    theAmazingTozziDuckMachine.address
-  );
+  console.log("🦆 Duck Machine Deployed To:", theAmazingTozziDuckMachine.address);
+  console.log("Initial Config: ", defaultConfig);
 }
 
 main()
