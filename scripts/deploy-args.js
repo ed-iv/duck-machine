55930394a99e35fb3c92bed1a8e5f74001e6abf2
Ed Henriques IV <ehenriquesiv@gmail.com>
2022-08-19 07:16:03 -0700

    update deploy args

--- scripts/deploy-args.js
+++ scripts/deploy-args.js
@@ -1,10 +1,10 @@
 module.exports = [
   {
-    tozziDuckPrice: "1000000000000000", 
-    customDuckPrice: "1000000000000000",
-    maxCustomDucks: "10",
-    tozziDuckMintStatus: "0",
-    customDuckMintStatus: "0",
+    tozziDuckPrice: "1000000000000000000", 
+    customDuckPrice: "1000000000000000000",
+    maxCustomDucks: "16",
+    tozziDuckMintStatus: "1",
+    customDuckMintStatus: "1",
   },
   "https://ipfs.io/ipfs/QmS2ypEqkaddZVTix8mRe3f14DJktYfAQ7ibkgnhcFk967"
 ];
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,16 +7,16 @@ const MINTSTATUS_DISABLED = BigNumber.from(1);
 const MINTSTATUS_WHITELIST = BigNumber.from(2);
 
 const defaultConfig = {
-  tozziDuckPrice: utils.parseEther("0.001"), 
-  customDuckPrice: utils.parseEther("0.001"),
-  maxCustomDucks: BigNumber.from(10),
-  tozziDuckMintStatus: MINTSTATUS_ENABLED,
-  customDuckMintStatus: MINTSTATUS_ENABLED,
+  tozziDuckPrice: utils.parseEther("1"), 
+  customDuckPrice: utils.parseEther("1"),
+  maxCustomDucks: BigNumber.from(16),
+  tozziDuckMintStatus: MINTSTATUS_DISABLED,
+  customDuckMintStatus: MINTSTATUS_DISABLED,
 };
 
 async function main() {  
   const TheAmazingTozziDuckMachine = await ethers.getContractFactory("TheAmazingTozziDuckMachine");
-  const theAmazingTozziDuckMachine = await TheAmazingTozziDuckMachine.deploy(defaultConfig,"foo");
+  const theAmazingTozziDuckMachine = await TheAmazingTozziDuckMachine.deploy(defaultConfig, "https://ipfs.io/ipfs/QmS2ypEqkaddZVTix8mRe3f14DJktYfAQ7ibkgnhcFk967");
   await theAmazingTozziDuckMachine.deployed();
   console.log("🦆 Duck Machine Deployed To:", theAmazingTozziDuckMachine.address);
   console.log("Initial Config: ", defaultConfig);
